48c89e0f1bc53635aaecee923f07c541ddbf6153
Jake Worsham <jworsham9697@gmail.com>
2025-04-12 22:27:39 -0400

    resolved deployment errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"; 
-import styles from "./page.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Container} from "react-bootstrap"
 import ModernChatInterface from "@/components/ModernChatInterface";
